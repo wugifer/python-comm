92437b4cfb46606e918e793be1942b90421e46e7
wg <wg@>
2023-06-30 14:17:20 +0800

    增加 to_m 接口

--- src/more_error.rs
+++ src/more_error.rs
@@ -1,83 +1,72 @@
 use std::{error::Error, fmt};
 
-/// 包含更多信息的 Error
+/// 包含更多信息的 Error: 每次调用的文件名、行号、函数名、可选的附加内容
 pub struct MoreError {
-    simple_text: String,
-    detail_text: String,
+    text: String,
 }
 
 impl MoreError {
-    /// 详细内容
-    pub fn detail(&self) -> &String {
-        &self.detail_text
-    }
-
     /// 从 Error 构造
     fn from_error<E>(err: &E, file: &str, line: u32, func: &str, text: &str) -> Self
     where
         E: Error,
     {
         Self {
-            simple_text: format!("Error: {} {}()\nError: {}", file, func, err),
-            detail_text: format!("Error: {}:{:3} {}() {}\nError: {:?}", file, line, func, text, err),
+            text: format!("Error: {}:{:3} {}() {}\nError: {:?}", file, line, func, text, err),
         }
     }
 
     /// 从 MoreError 构造
     fn from_more(err: &Self, file: &str, line: u32, func: &str, text: &str) -> Self {
         Self {
-            simple_text: format!("Error: {} {}()\n{}", file, func, err.simple()),
-            detail_text: format!("Error: {}:{:3} {}() {}\n{}", file, line, func, text, err.detail()),
+            text: format!("Error: {}:{:3} {}() {}\n{}", file, line, func, text, err.text),
         }
     }
 
     /// 从零构造
     pub fn new(file: &str, line: u32, func: &str, text: &str) -> Self {
         Self {
-            simple_text: format!("Error: {} {}()", file, func),
-            detail_text: format!("Error: {}:{:3} {}() {}", file, line, func, text),
+            text: format!("Error: {}:{:3} {}() {}", file, line, func, text),
         }
-    }
-
-    /// 简单内容
-    pub fn simple(&self) -> &String {
-        &self.simple_text
     }
 }
 
 impl fmt::Debug for MoreError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.simple().fmt(f)
+        self.text.fmt(f)
     }
 }
 
 impl fmt::Display for MoreError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.detail().fmt(f)
+        self.text.fmt(f)
     }
 }
 
-/// 给 Error 增加更多信息
+/// 给 Error, ... 增加更多信息
 pub trait AddMoreError<T> {
-    /// 附加文件名、行号、函数名、附加说明
+    /// 附加文件名、行号、函数名、附加说明, 生成 MoreError, 与 m() 类似, 但附加内容由闭包产生
     fn f<F>(self, file_line_func_func: (&str, u32, &str, F)) -> Result<T, MoreError>
     where
         F: Fn() -> String;
 
-    /// 附加文件名、行号、函数名、附加说明
+    /// 附加文件名、行号、函数名、附加说明, 生成 MoreError
     fn m(self, file_line_func_text: (&str, u32, &str, &str)) -> Result<T, MoreError>;
 
-    /// 附加文件名、行号、函数名、附加说明
+    /// 附加文件名、行号、函数名、附加说明, 输出
     fn p(&self, file_line_func_text: (&str, u32, &str, &str));
 }
 
-/// 把 Error 转换为简单的 String MoreError
+/// 给 Error, ... 增加更多信息, 但抛弃 Error, ... 自身的内容
 pub trait AsMoreError<T> {
     /// 附加文件名、行号、函数名、附加说明, 抛弃 Error 自身的内容
     fn as_m(self, file_line_func_text: (&str, u32, &str, &str)) -> Result<T, MoreError>;
+
+    /// 转化为 MoreError, 不附加信息
+    fn to_m(self) -> Result<T, MoreError>;
 }
 
-/// 合并外层的 MoreError 到内层
+/// 合并 Result<Result<T, E>, E> 为 Result<T, E>
 pub trait LessError<T, E> {
     /// Ok(Result) -> Result, Err(*) -> Err(*)
     fn l(self) -> Result<T, E>;
@@ -92,27 +81,27 @@ where
     where
         F: Fn() -> String,
     {
-        self.or_else(|err| {
-            Err(MoreError::from_error(
+        self.map_err(|err| {
+            MoreError::from_error(
                 &err,
                 file_line_func_func.0,
                 file_line_func_func.1,
                 file_line_func_func.2,
                 &file_line_func_func.3(),
-            ))
+            )
         })
     }
 
     /// 附加文件名、行号、函数名、附加说明
     fn m(self, file_line_func_text: (&str, u32, &str, &str)) -> Result<T, MoreError> {
-        self.or_else(|err| {
-            Err(MoreError::from_error(
+        self.map_err(|err| {
+            MoreError::from_error(
                 &err,
                 file_line_func_text.0,
                 file_line_func_text.1,
                 file_line_func_text.2,
                 file_line_func_text.3,
-            ))
+            )
         })
     }
 
@@ -128,7 +117,6 @@ where
                     file_line_func_text.2,
                     file_line_func_text.3,
                 )
-                .detail()
             );
         }
     }
@@ -174,7 +162,6 @@ where
                 file_line_func_text.2,
                 file_line_func_text.3,
             )
-            .detail()
         );
     }
 }
@@ -185,27 +172,27 @@ impl<T> AddMoreError<T> for Result<T, MoreError> {
     where
         F: Fn() -> String,
     {
-        self.or_else(|err| {
-            Err(MoreError::from_more(
+        self.map_err(|err| {
+            MoreError::from_more(
                 &err,
                 file_line_func_func.0,
                 file_line_func_func.1,
                 file_line_func_func.2,
                 &file_line_func_func.3(),
-            ))
+            )
         })
     }
 
     /// 附加文件名、行号、函数名、附加说明
     fn m(self, file_line_func_text: (&str, u32, &str, &str)) -> Result<T, MoreError> {
-        self.or_else(|err| {
-            Err(MoreError::from_more(
+        self.map_err(|err| {
+            MoreError::from_more(
                 &err,
                 file_line_func_text.0,
                 file_line_func_text.1,
                 file_line_func_text.2,
                 file_line_func_text.3,
-            ))
+            )
         })
     }
 
@@ -221,7 +208,6 @@ impl<T> AddMoreError<T> for Result<T, MoreError> {
                     file_line_func_text.2,
                     file_line_func_text.3,
                 )
-                .detail()
             );
         }
     }
@@ -264,7 +250,6 @@ impl<T> AddMoreError<T> for &MoreError {
                 file_line_func_text.2,
                 file_line_func_text.3,
             )
-            .detail()
         );
     }
 }
@@ -273,15 +258,22 @@ impl<T, E> AsMoreError<T> for Result<T, E>
 where
     E: Error,
 {
-    /// 把 Error 转换为简单的 String MoreError
+    /// 附加文件名、行号、函数名、附加说明, 抛弃 Error 自身的内容
     fn as_m(self, file_line_func_text: (&str, u32, &str, &str)) -> Result<T, MoreError> {
-        self.or_else(|_| {
-            Err(MoreError::new(
+        self.map_err(|_| {
+            MoreError::new(
                 file_line_func_text.0,
                 file_line_func_text.1,
                 file_line_func_text.2,
                 file_line_func_text.3,
-            ))
+            )
+        })
+    }
+
+    /// 转化为 MoreError, 不附加信息
+    fn to_m(self) -> Result<T, MoreError> {
+        self.map_err(|err| MoreError {
+            text: format!("{:?}", err),
         })
     }
 }
